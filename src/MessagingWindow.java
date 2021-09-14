674f22354b74bc0ae0abde6b4e6b6372bd4fce25
charl <carlos.nunez6@upr.edu>
2021-09-14 17:47:36 -0500

    Added a JTextArea so that the user can write, the program currently only prints the text once the Send button is pressed in the future this command will be exchanged with sending the text to the server.

--- src/MessagingWindow.java
+++ src/MessagingWindow.java
@@ -3,19 +3,25 @@ import java.awt.*;
 
 public class MessagingWindow extends JFrame {
 
+    JTextArea messageField = new JTextArea();
+
+
+
+
     MessagingWindow(){
         ImageIcon imageIcon = new ImageIcon("C:\\Users\\charl\\git\\PrivateMessagingNetwork\\src\\Images/server.png");
         this.setTitle("Messaging Window");  // Sets title for window.
         this.setIconImage(imageIcon.getImage());
-        this.setResizable(false);
         this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);  // Makes the frame close on command.
         this.getContentPane().setBackground(new Color(102,102,102));  // Sets color of the background of the frame.
-        this.setLayout(null);
         this.add(serverConnection());
         this.add(returnButton());
         this.add(sendMessageButton());
+        this.add(messageHistory());
+
         this.add(makeJPanel(800,800,75,50,Color.GRAY));
-        this.add(makeJPanel(75,640,0,50,Color.RED));
+        this.pack();
+
 
         this.setVisible(true);  // Makes frame appear on screen.
         this.setSize(500,640); // Sets the shape of frame.
@@ -30,10 +36,7 @@ public class MessagingWindow extends JFrame {
     }
 
     public JPanel returnButton(){
-        //ImageIcon imageIcon = new ImageIcon("C:\\Users\\charl\\git\\PrivateMessagingNetwork\\src\\Images/return.png");
-
         JButton returnButton = new JButton();
-        //returnButton.setIcon(imageIcon);
         returnButton.setText("Return");
         returnButton.setFocusable(false);
         returnButton.setBounds(0,0,75,50);
@@ -46,7 +49,7 @@ public class MessagingWindow extends JFrame {
 
     public JPanel serverConnection(){
         JLabel messagingTitle = new JLabel();
-        messagingTitle.setLayout(null);
+
         messagingTitle.setBounds(60,0,500,50);
         messagingTitle.setText("Connection ::: ServerToConnect");
         messagingTitle.setFont(new Font("Daytona", Font.BOLD, 20));
@@ -55,6 +58,7 @@ public class MessagingWindow extends JFrame {
 
         JPanel serverConnectionLabel = makeJPanel(500,50,75,0,null);
         serverConnectionLabel.add(messagingTitle);
+
         return serverConnectionLabel;
     }
 
@@ -66,21 +70,34 @@ public class MessagingWindow extends JFrame {
     public JPanel sendMessageButton(){
 
         JButton messageButton = new JButton();
+        messageField.setPreferredSize(new Dimension(250,40));
+        messageField.setBounds(75,0,345,100);
+        messageField.setFont(new Font("Daytona", Font.BOLD, 15));
 
         messageButton.setText("Send");
         messageButton.setFocusable(false);
         messageButton.setBounds(0,0,75,90);
         messageButton.addActionListener(e->sendMessage());
 
-        JPanel messagePanel = makeJPanel(420,100,75,520,Color.BLACK);
-        messagePanel.setLayout(null);
+        JPanel messagePanel = makeJPanel(420,100,75,520, new Color(224,224,224 ));
+
+        messagePanel.add(messageField);
         messagePanel.add(messageButton);
+
         return messagePanel;
     }
 
     public void sendMessage(){
-        System.out.println("Sending Message Now");
+        System.out.println(messageField.getText());
+        messageField.setText("");
     }
+
+    public JPanel messageHistory(){
+        JPanel textHistory = makeJPanel(75,640,0,50, new Color(90, 90, 120));
+
+        return textHistory;
+    }
+
 
 
 
