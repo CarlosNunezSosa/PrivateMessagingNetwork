33c3d69bbd627035943b36a75cd4d34b6befcf53
charl <carlos.nunez6@upr.edu>
2021-09-22 02:48:48 -0500

    Added comments to make the code more readable, and to organize code.

--- src/Menu.java
+++ src/Menu.java
@@ -2,64 +2,68 @@ import javax.swing.*;
 import java.awt.*;
 
 
-
 public class Menu extends JFrame{
+
+        // Main Window for th Program.
         Menu(){
-            ImageIcon imageIcon = new ImageIcon("C:\\Users\\charl\\git\\PrivateMessagingNetwork\\src\\Images/server.png");
+            ImageIcon imageIcon = new ImageIcon("C:\\Users\\charl\\git\\PrivateMessagingNetwork\\src\\Images/server.png"); // Saves teh Image for Icon and Logo.
             this.setTitle("Private Home Network");  // Sets title for window.
-            this.setIconImage(imageIcon.getImage());
+            this.setIconImage(imageIcon.getImage()); // Sets the saved image as the Window Icon.
             this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);  // Makes the frame close on command.
             this.setResizable(false);  // Makes the frame non resizable.
-            this.add(title_of_program());
-            this.add(button());
+            this.add(title_of_program()); // Adds the Title to the Menu JFrame.
+            this.add(button());// Adds the Connect Button to the Menu JFrame.
             this.getContentPane().setBackground(new Color(102,102,102));  // Sets color of the background of the frame.
-            this.setLayout(null);
+            this.setLayout(null); // Sets layout to null.
             this.setVisible(true);  // Makes frame appear on screen.
             this.setSize(500,500); // Sets the shape of frame.
-
         }
 
-
+        // Creates Title for the Program.
         public JLabel title_of_program(){
+            // Image for Title.
+            ImageIcon imageIcon = new ImageIcon("C:\\Users\\charl\\git\\PrivateMessagingNetwork\\src\\Images/server.png"); // Sets the Image for Title.
+            Image image = imageIcon.getImage(); // Converts ImageIcon to Image.
+            Image icon = image.getScaledInstance(120,120, Image.SCALE_SMOOTH); // Scales the Image to a manageable size.
+            imageIcon = new ImageIcon(icon); // Converts Image back to ImageIcon.
 
-            ImageIcon imageIcon = new ImageIcon("C:\\Users\\charl\\git\\PrivateMessagingNetwork\\src\\Images/server.png");
-            Image image = imageIcon.getImage();
-            Image icon = image.getScaledInstance(120,120, Image.SCALE_SMOOTH);
-            imageIcon = new ImageIcon(icon);
-
-            JLabel title = new JLabel();
-            title.setBounds(120,0,250,250);
-            title.setText("Private Home Network");
-            title.setIcon(imageIcon);
-            title.setHorizontalTextPosition(JLabel.CENTER);
-            title.setVerticalTextPosition(JLabel.TOP);
-            title.setFont(new Font("Daytona", Font.BOLD, 20));
-            title.setVerticalAlignment(JLabel.CENTER);
-            title.setHorizontalAlignment(JLabel.CENTER);
-
-            return title;
+            // Title for Menu
+            JLabel title = new JLabel(); // Creates the Title JLabel.
+            title.setBounds(120,0,250,250); // Sets the bounds for the Title.
+            title.setText("Private Home Network"); // Sets the text for the Title.
+            title.setIcon(imageIcon); // Sets the ImageIcon for the Title.
+            title.setHorizontalTextPosition(JLabel.CENTER); // Sets the horizontal position for Text.
+            title.setVerticalTextPosition(JLabel.TOP); // Sets the vertical position for Text.
+            title.setFont(new Font("Daytona", Font.BOLD, 20)); // Sets the Font for the Title.
+            title.setVerticalAlignment(JLabel.CENTER); // Aligns the Title Vertically.
+            title.setHorizontalAlignment(JLabel.CENTER); // Aligns the Title Horizontally.
+            return title; // Returns the Title with the modified changes.
         }
 
+        // Connect Button.
         public JPanel button(){
-            JPanel panel = new JPanel();
-            panel.setLayout(null);
-            panel.setBounds(120,230,250,100);
-            panel.setBackground(new Color(102,102,102));
+            // Connect Button
+            JButton connectButton = new JButton(); // Creates the Connect Button.
+            connectButton.setText("Connect"); // Sets the Text of the Button to Connect.
+            connectButton.setFont(new Font("Daytona", Font.BOLD, 10)); // Sets the Font for the Button.
+            connectButton.setFocusable(false); // Makes the Button un-focusable.
+            connectButton.setBounds(65,25,125,50); // Sets the boundaries for the button.
+            connectButton.addActionListener(e -> action()); // Sets the action for when the button is pressed. Calls action().
 
-            JButton connectButton = new JButton();
-            connectButton.setText("Connect");
-            connectButton.setFont(new Font("Daytona", Font.BOLD, 10));
-            connectButton.setFocusable(false);
-            connectButton.setBounds(65,25,125,50);
-            connectButton.addActionListener(e -> action());
-
-            panel.add(connectButton);
-            return panel;
+            // Panel for Button.
+            JPanel panel = new JPanel(); // Creates the Panel for the Button.
+            panel.setLayout(null); // Sets teh Layout to null.
+            panel.setBounds(120,230,250,100); // Sets the boundaries for the JPanel.
+            panel.setBackground(new Color(102,102,102)); // Sets teh Color background for the Panel.
+            panel.add(connectButton); // Adds the Button to the Panel.
+            return panel; // Return the Panel with the Button included.
         }
 
+        // Connection Action.
         public void action(){
-            MessagingWindow myMessageSystem = new MessagingWindow();
-            this.dispose();
+            // Action
+            MessagingWindow myMessageSystem = new MessagingWindow(); // Creates a new Window called myMessageSystem.
+            this.dispose(); // Closes current Window.
         }
 
 
